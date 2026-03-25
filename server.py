2fd6f5062d8277fa7097a0a6a5c22c8620e42c29
imirandax <138390006+imirandax@users.noreply.github.com>
2026-03-25 13:25:51 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 from flask import Flask
 import subprocess
+import os
 
 app = Flask(__name__)
 
 @app.route('/ejecutar')
 def ejecutar():
     print("🔥 Ejecutando en servidor nube")
-
     subprocess.Popen(["python", "script_final.py"])
-
     return "OK", 200
 
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
